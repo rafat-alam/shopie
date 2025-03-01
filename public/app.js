f61be9ca58062370125acd4d8ee52629a043c190
Rafat Alam <Rafat.Alam.RA@gmail.com>
2025-03-01 17:01:55 +0530

    update

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
-let button = document.querySelector('button');
+let button = document.querySelector("button");
 
-button.addEventListener('click', () => {
-  let input = document.querySelector('input');
+button.addEventListener("click", () => {
+  let input = document.querySelector("input");
   let value = input.value;
-  let ip = '192.168.29.249';
+  let ip = "192.168.29.249";
   let port = 3000;
   let link = `http://${ip}:${port}/${value}`;
 
   window.open(link, "_blank");
 
-  input.value = '';
+  input.value = "";
 });
--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const port = 3000;
 
 const users = {
-  "rafat":    { likes: 10, followers: 50, following: 30 },
-  "shubham":  { likes: 25, followers: 80, following: 60 },
-  "radhey":   { likes: 5, followers: 20, following: 15 },
-  "sanskar":  { likes: 15, followers: 60, following: 45 },
-  "yash":     { likes: 8, followers: 40, following: 25 },
-  "dev":      { likes: 20, followers: 90, following: 70 },
-  "prem":     { likes: 12, followers: 55, following: 40 }
+  rafat: { likes: 10, followers: 50, following: 30 },
+  shubham: { likes: 25, followers: 80, following: 60 },
+  radhey: { likes: 5, followers: 20, following: 15 },
+  sanskar: { likes: 15, followers: 60, following: 45 },
+  yash: { likes: 8, followers: 40, following: 25 },
+  dev: { likes: 20, followers: 90, following: 70 },
+  prem: { likes: 12, followers: 55, following: 40 },
 };
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/:u', (req, res) => {
-    console.log('Request received for:', req.params.u);
-    const { u } = req.params;
+app.get("/:u", (req, res) => {
+  console.log("Request received for:", req.params.u);
+  const { u } = req.params;
 
-    if (users[u]) {
-      res.render('found.ejs', {u, likes : users[u].likes, followers : users[u].followers, following : users[u].following});
-    } else {
-      res.render('notfound.ejs', {u});
-    }
+  if (users[u]) {
+    res.render("found.ejs", {
+      u,
+      likes: users[u].likes,
+      followers: users[u].followers,
+      following: users[u].following,
+    });
+  } else {
+    res.render("notfound.ejs", { u });
+  }
+});
+
+app.get("*", (req, res) => {
+  console.log("Request received for:", req.params[0]);
+  res.render("accessdenied.ejs");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
